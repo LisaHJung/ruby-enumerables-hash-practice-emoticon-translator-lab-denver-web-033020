4b105787f9f92ea91af05ba447d7c23e6c5fdb3c
Lisa Jung <lisahjung@gmail.com>
2020-03-18 22:51:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,9 +3,7 @@ require "pry"
 
  def load_library(file_path)
    library = YAML.load_file(file_path)
-    binding.pry
    result = {"get_meaning" => {}, "get_emoticon" => {}}
-  
   library.each do |meaning, emoticons|
     result["get_meaning"][emoticons[1]] = meaning
     result["get_emoticon"][emoticons[0]] = emoticons[1]
