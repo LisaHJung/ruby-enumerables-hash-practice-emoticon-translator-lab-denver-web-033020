9e8cee38664a0b6b9375b60b88d231516bf79380
Lisa Jung <lisahjung@gmail.com>
2020-03-18 23:24:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,6 @@ end
 
 
   def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
-    binding.pry
   library = load_library(file_path)
   if library["get_emoticon"].include?(english_emoticon) 
     library["get_emoticon"][english_emoticon]
